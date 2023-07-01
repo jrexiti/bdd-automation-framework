44de8a17c8a808a10d6289160fb399096070cc8d
jrexiti <jay.rexiti@gmail.com>
2023-07-01 17:00:22 -0400

    ran a dummy test, the setup works

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        WebDriver driver;
+
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver");
+
+        driver = new ChromeDriver();
+
+        driver.get("https:www.google.com");
+
+
     }
-}
+
+}  
