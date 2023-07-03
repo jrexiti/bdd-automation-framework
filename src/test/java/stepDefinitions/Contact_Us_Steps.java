f69977d988d1262b547006a1c26969b60fd5b278
jrexiti <jay.rexiti@gmail.com>
2023-07-03 16:46:24 -0400

    added step definition

---
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -0,0 +1,54 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Contact_Us_Steps {
+    @Given("user navigates to university contact us Page")
+    public void user_navigates_to_university_contact_us_page() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("Hi");
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user enters a unique first name")
+    public void user_enters_a_unique_first_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("user enters a unique last name")
+    public void user_enters_a_unique_last_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("user enters a unique email")
+    public void user_enters_a_unique_email() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @And("user clicks on the submit button")
+    public void user_clicks_on_the_submit_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user should be presented with a successful contact us submission message")
+    public void user_should_be_presented_with_a_successful_contact_us_submission_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+
+
+
+
+
+}
